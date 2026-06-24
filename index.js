6b60849e62f3292c942819a606276a23986d6e8e
Aditya Yadav <217367477+adityayadav176@users.noreply.github.com>
2026-06-24 15:15:16 +0530

    fix: update CORS origin settings to include production and local environments

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const PORT = process.env.PORT || 5000;
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:3000", "http://127.0.0.1:5500/first.html"],
+    origin: [
+      "https://work-space-pro-frontend-l8bx-fq5mp9i3r.vercel.app", 
+      "http://localhost:3000",
+      "http://localhost:5173"
+    ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
 );
